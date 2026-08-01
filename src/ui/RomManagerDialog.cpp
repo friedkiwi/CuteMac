dffb04520968284c1d9e495d8bb90e702e9ab096
Yvan Janssens <yvanj@cyber.gent>
2026-08-01 21:49:25 +0200

    Simplify ROM manager overview

--- src/ui/RomManagerDialog.cpp
+++ src/ui/RomManagerDialog.cpp
@@ -18,14 +18,16 @@ RomManagerDialog::RomManagerDialog(QWidget* parent)
     : QDialog(parent)
 {
     setWindowTitle(QStringLiteral("ROM Manager"));
-    resize(760, 430);
+    resize(900, 430);
     auto* layout = new QVBoxLayout(this);
-    m_table = new QTableWidget(0, 5);
-    m_table->setHorizontalHeaderLabels({ QStringLiteral("Found"), QStringLiteral("ROM"), QStringLiteral("Revision"), QStringLiteral("File"), QStringLiteral("Checksum") });
+    m_table = new QTableWidget(0, 4);
+    m_table->setHorizontalHeaderLabels({ QStringLiteral("Found"), QStringLiteral("ROM"), QStringLiteral("Revision"), QStringLiteral("File") });
     m_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
     m_table->setSelectionBehavior(QAbstractItemView::SelectRows);
     m_table->verticalHeader()->hide();
+    m_table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
     m_table->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+    m_table->horizontalHeader()->setSectionResizeMode(2, QHeaderView::ResizeToContents);
     m_table->horizontalHeader()->setSectionResizeMode(3, QHeaderView::Stretch);
     layout->addWidget(m_table, 1);
 
@@ -57,14 +59,8 @@ void RomManagerDialog::refresh()
         m_table->setItem(row, 2, new QTableWidgetItem(status.definition.revision));
         m_table->setItem(row, 3, new QTableWidgetItem(status.definition.kind == rom::RomKind::Embedded
                 ? QStringLiteral("(embedded)") : (status.found ? QFileInfo(status.path).fileName() : QStringLiteral("(missing)"))));
-        const auto checksum = !status.definition.sha256.isEmpty() ? status.definition.sha256
-            : (!status.definition.sha1.isEmpty() ? status.definition.sha1 : status.definition.headerChecksum);
-        m_table->setItem(row, 4, new QTableWidgetItem(QString::fromLatin1(checksum.toHex())));
         if (status.definition.discouraged) m_table->item(row, 2)->setToolTip(QStringLiteral("Older revision with known bugs; v3 is recommended."));
     }
-    m_table->resizeColumnToContents(0);
-    m_table->resizeColumnToContents(2);
-    m_table->resizeColumnToContents(4);
 }
 
 } // namespace cutemac::ui
